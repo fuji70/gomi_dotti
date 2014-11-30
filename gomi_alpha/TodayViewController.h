7c85e6045829e02b5ec649390cd9fb7905a9a4d0
fuji70 <fujifuji70@gmail.com>
2014-11-30 15:38:43 +0900

    add Subclass to control 'Today'. but not implemented yet.

---
+++ gomi_alpha/TodayViewController.h
@@ -0,0 +1,14 @@
+//
+//  TodayViewController.h
+//  gomi_alpha
+//
+//  Created by Naoki on 2014/11/30.
+//  Copyright (c) 2014年 vidacomoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+- (IBAction)doRefresh:(id)sender;
+
+@end
