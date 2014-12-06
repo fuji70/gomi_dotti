dc81db700e549ef9782e53ea169ea7832e1521f4
fuji70 <fujifuji70@gmail.com>
2014-12-06 17:08:03 +0900

    設定で8ブロックのボタンのみ設定取り込みを実装。表示への反映はまだ。

---
+++ gomi_alpha/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  gomi_alpha
+//
+//  Created by Naoki on 2014/12/06.
+//  Copyright (c) 2014年 vidacomoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+- (IBAction)btnBlk8:(id)sender;
+
+@end
---
+++ gomi_alpha/SettingViewController.m
@@ -0,0 +1,51 @@
+//
+//  SettingViewController.m
+//  gomi_alpha
+//
+//  Created by Naoki on 2014/12/06.
+//  Copyright (c) 2014年 vidacomoda. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@interface SettingViewController ()
+
+@end
+
+@implementation SettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)setBlkNum:(int) blkNum {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:blkNum forKey:@"blkNum"];
+    BOOL successful = [defaults synchronize];
+    if (successful) {
+        NSLog(@"set to blkNum: %d.", blkNum);
+    }
+    
+}
+
+- (IBAction)btnBlk8:(id)sender {
+    int blkNum=8;
+    [self setBlkNum:blkNum];
+}
+@end
