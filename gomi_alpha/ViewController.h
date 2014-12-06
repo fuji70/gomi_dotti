249273c80c99e3e24c5062ba43a36c383128ebf9
岩崎正則 <m.rock77.mobile@gmail.com>
2014-12-06 16:31:52 +0900

    種類検索画面のレイアウト修正と”品名検索”部分をタッチするとwebviewを開く機能を追加 レイアウトは出力部の色分け、電話番号の表記、これらを追加したことによる上部の寸法など修正。

--- gomi_alpha/ViewController.h
+++ gomi_alpha/ViewController.h
@@ -9,6 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+<UIPickerViewDelegate,UIPickerViewDataSource> {
+    IBOutlet UIPickerView *pickerView;
+    IBOutlet UILabel *labelNunber;
+    
+    IBOutlet UIWebView *webView1;//藤沢市ごみ分別HP用
+    IBOutlet UIWebView *webView2;//藤沢市ブロック検索HP用
+}
 @end
 
--- gomi_alpha/ViewController.m
+++ gomi_alpha/ViewController.m
@@ -24,6 +24,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSURLRequest* req1 = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.google.co.jp/"]]; [webView1 loadRequest:req1];
+//    NSURLRequest* req1 = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://fj4.city.fujisawa.kanagawa.jp/fdust/"]]; [webView1 loadRequest:req1];
+    NSURLRequest* req2 = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.city.fujisawa.kanagawa.jp/kankyo-j/kurashi/gomi/shushubi/h2603-h2703/jusho-a-ta.html"]]; [webView2 loadRequest:req2];
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,6 +35,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload { }
 
+// web を解放
+- (void)dealloc1 { webView1.delegate = nil; }
+- (void)dealloc2 { webView2.delegate = nil; }
 
 @end
