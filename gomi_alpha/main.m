491aae4f603351d48317fbeb6c6a3070a172bec4
fuji70 <fujifuji70@gmail.com>
2014-12-10 02:14:04 +0900

    落ちる前にNSLogで理由を出力するようにデバッグ用追加。

--- gomi_alpha/main.m
+++ gomi_alpha/main.m
@@ -13,6 +13,11 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        @try {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        }
+        @catch (NSException * exception) {
+            NSLog(@"%@", exception);
+        }
     }
 }
