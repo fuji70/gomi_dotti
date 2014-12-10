509caff293dd10a09172565659f7cb7078b13544
fuji70 <fujifuji70@gmail.com>
2014-12-11 00:49:18 +0900

    作業中

---
+++ gomi_alpha/WebAddressSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebAddressSearchViewController.h
+//  gomi_alpha
+//
+//  Created by Naoki on 2014/12/11.
+//  Copyright (c) 2014年 vidacomoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebAddressSearchViewController : UIViewController
+
+@end
