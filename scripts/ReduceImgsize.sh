6f6f679360e98423c17442d014c26a1e135b16f5
fuji70 <fujifuji70@gmail.com>
2015-01-17 16:05:06 +0900

    reduce color.

--- scripts/ReduceImgsize.sh
+++ scripts/ReduceImgsize.sh
@@ -4,4 +4,4 @@ TARDIR=../gomi_alpha/image4app
 
 ls ${TARDIR}/*blk-*.png | parallel mogrify -verbose -resize x750 {}
 ls ${TARDIR}/*-list.png | parallel mogrify -verbose -resize x750 {}
-
+ls ${TARDIR}/*.png | parallel pngquant -v --ext .png --force 256 {}
