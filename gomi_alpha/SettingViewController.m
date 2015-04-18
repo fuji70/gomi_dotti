8d94844e55488669e51b3197cc06dd5adf6c3af8
岩崎正則 <m.rock77.mobile@gmail.com>
2015-04-19 03:19:30 +0900

    住所設定のボタンのリンクを修正し８番地区までに修正 ただしボタンサイズが正しくない

--- gomi_alpha/SettingViewController.m
+++ gomi_alpha/SettingViewController.m
@@ -46,7 +46,12 @@
     [HandleDb setBlkNum:blkNum];
     _lblBlknum.text = [NSString stringWithFormat:@"%d", [HandleDb getBlkNum]];
 }
-
+/*
+- (IBAction)btnBlk8:(id)sender {
+    int blkNum=1;
+    [self setBlkNum:blkNum];
+}
+*/
 - (IBAction)btnBlk1:(id)sender {
     int blkNum=1;
     [self setBlkNum:blkNum];
@@ -66,7 +71,12 @@
     int blkNum=4;
     [self setBlkNum:blkNum];
 }
-
+/*
+- (IBAction)btnBlk6:(id)sender {
+    int blkNum=6;
+    [self setBlkNum:blkNum];
+}
+*/
 - (IBAction)btnBlk5:(id)sender {
     int blkNum=5;
     [self setBlkNum:blkNum];
@@ -84,16 +94,6 @@
 
 - (IBAction)btnBlk8:(id)sender {
     int blkNum=8;
-    [self setBlkNum:blkNum];
-}
-
-- (IBAction)btnBlk9:(id)sender {
-    int blkNum=9;
-    [self setBlkNum:blkNum];
-}
-
-- (IBAction)btnBlk10:(id)sender {
-    int blkNum=10;
     [self setBlkNum:blkNum];
 }
 
