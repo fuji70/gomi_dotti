c82f5f3925f3e167cbeba41237579f083b87ca9d
岩崎正則 <m.rock77.mobile@gmail.com>
2018-10-21 17:20:56 +0900

    HandleDb.mに収集場所表示の修正を追加(藤沢ごみからコピペ)

--- gomi_alpha/HandleDb.h
+++ gomi_alpha/HandleDb.h
@@ -11,15 +11,19 @@
 @interface HandleDb : NSObject
 
 + (NSString*)getIconsStr:(NSDate*)date;
++ (NSString*)getPitIconsStr:(NSDate*)date;//181014 捨て場所表示
 + (NSDate*)getNextDate:(NSString*)iconsStr startDate:(NSDate*)startDate;
 + (UIImage*)getIconImage:(NSString*)iconsStr;
++ (UIImage*)getPitIconImage:(NSString*)PiticonsStr;//181014 捨て場所表示
 + (UIImage*)getWordIconImage:(NSString*)iconsStr;
 + (UIImage*)getDateIconImage:(NSString*)iconsStr;
++ (UIImage*)getDatePitIconImage:(NSString*)PiticonsStr;//181014 捨て場所表示
 + (UIImage*)getIconImageFromDate:(NSDate*)date;
-+ (UIImage*)getBlock:(NSDate*)iconsStr;// 20181016 blk名
++ (UIImage*)getPitIconImageFromDate:(NSDate*)date;//181014 捨て場所表示
 + (NSString*)getPitStr:(NSString*)iconsStr;
 + (NSString*)getSpeechStr:(NSString*)iconsStr;
 + (UIImage*)getMonthImage:(NSDate*)date;
++ (UIImage*)getBlock:(NSDate*)iconsStr;// 20181016 blk名
 
 + (id)getInstance;
 
